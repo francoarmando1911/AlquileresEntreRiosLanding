cdc3ef5d75609155e253e7da076a3b503c774dc6
Franco Armando <francoarmando1911@gmail.com>
2025-09-09 19:37:54 -0300

    Creacion de componentes para landing page

---
+++ src/components/PublishSection.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export default function PublishSection() {
+    return (
+        <section className="w-full py-20 bg-slate-200 flex justify-center items-center">
+            <h2 className="text-4xl font-bold text-center text-secondary mb-12">
+                ¿Querés publicar tu propiedad en Alquileres Entre Ríos?
+            </h2>
+        </section>
+    );
+}
