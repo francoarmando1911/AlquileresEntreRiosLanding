058a72c9c39f9aa8860c274da4bb929297009801
Franco Armando <francoarmando1911@gmail.com>
2025-09-09 19:53:29 -0300

    Actualizacion de medidas

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -15,10 +15,10 @@ export default function HeroSection() {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8 }}
         >
-            {/* Fondo */}
+
             <div className="absolute inset-0">
                 <Image
-                    src="/images/concordia2.png"
+                    src="/images/concordia.png"
                     alt="Fondo hero"
                     fill
                     className="object-cover opacity-40"
@@ -26,7 +26,6 @@ export default function HeroSection() {
                 />
             </div>
 
-            {/* Contenido */}
             <motion.h1
                 className="text-5xl md:text-7xl font-extrabold text-secondary mb-4 relative z-10"
                 variants={itemVariants}
@@ -34,7 +33,7 @@ export default function HeroSection() {
                 Tus Próximas Vacaciones te Esperan
             </motion.h1>
             <motion.p
-                className="text-lg md:text-xl text-gray-600 mb-8 relative z-10"
+                className="text-lg md:text-xl text-gray-700 mb-8 relative z-10"
                 variants={itemVariants}
             >
                 La forma más sencilla de encontrar alquileres en Entre Ríos.
